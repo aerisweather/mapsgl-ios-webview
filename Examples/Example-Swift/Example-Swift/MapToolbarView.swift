1605c079bb3eae55772b6eceb2b7d4576baa72ec
Nicholas Shipes <nshipes@urban10.com>
2022-10-24 11:12:54 -0700

    chore: move MapToolbarView to separate file

---
+++ Examples/Example-Swift/Example-Swift/MapToolbarView.swift
@@ -0,0 +1,52 @@
+//
+//  MapToolbarView.swift
+//  Example-Swift
+//
+//  Created by Nicholas Shipes on 10/24/22.
+//
+
+import UIKit
+
+class MapToolbarView: UIView {
+    let playButton = UIButton(type: .custom)
+    let timeLabel = UILabel()
+    let activityIndicator = UIActivityIndicatorView(style: .medium)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        layoutMargins = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+        layer.cornerRadius = 4
+        
+        let iconConfiguration = UIImage.SymbolConfiguration(scale: .large)
+        playButton.setImage(UIImage(systemName: "play.fill", withConfiguration: iconConfiguration), for: .normal)
+        playButton.setImage(UIImage(systemName: "stop.fill", withConfiguration: iconConfiguration), for: .selected)
+        playButton.tintColor = .white
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(playButton)
+        
+        activityIndicator.color = .white
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(activityIndicator)
+        
+        timeLabel.font = .preferredFont(forTextStyle: .callout)
+        timeLabel.textColor = .white
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(timeLabel)
+        
+        NSLayoutConstraint.activate([
+            playButton.leftAnchor.constraint(equalTo: layoutMarginsGuide.leftAnchor),
+            playButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            activityIndicator.leftAnchor.constraint(equalTo: playButton.rightAnchor, constant: 20),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: activityIndicator.rightAnchor, constant: 12),
+            timeLabel.rightAnchor.constraint(equalTo: layoutMarginsGuide.rightAnchor),
+            timeLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Examples/Example-Swift/Example-Swift/ViewController.swift
+++ Examples/Example-Swift/Example-Swift/ViewController.swift
@@ -10,43 +10,6 @@ import WebKit
 import CoreLocation
 import MapsGLWebView
 
-class MapToolbarView: UIView {
-    let playButton = UIButton(type: .custom)
-    let timeLabel = UILabel()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-        layoutMargins = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
-        layer.cornerRadius = 4
-        
-        let iconConfiguration = UIImage.SymbolConfiguration(scale: .large)
-        playButton.setImage(UIImage(systemName: "play.fill", withConfiguration: iconConfiguration), for: .normal)
-        playButton.setImage(UIImage(systemName: "stop.fill", withConfiguration: iconConfiguration), for: .selected)
-        playButton.tintColor = .white
-        playButton.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(playButton)
-        
-        timeLabel.font = .preferredFont(forTextStyle: .callout)
-        timeLabel.textColor = .white
-        timeLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(timeLabel)
-        
-        NSLayoutConstraint.activate([
-            playButton.leftAnchor.constraint(equalTo: layoutMarginsGuide.leftAnchor),
-            playButton.centerYAnchor.constraint(equalTo: centerYAnchor),
-            timeLabel.leftAnchor.constraint(equalTo: playButton.rightAnchor, constant: 20),
-            timeLabel.rightAnchor.constraint(equalTo: layoutMarginsGuide.rightAnchor),
-            timeLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 class ViewController: UIViewController {
     var mapView: MapsGLView!
     var legendView: MapsGLLegendView!
