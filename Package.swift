7b43271bfd3edb3396dfd645761d13bed73126fb
Nicholas Shipes <nshipes@urban10.com>
2022-11-07 07:16:02 -0800

    chore: update dependency version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Lision/WKWebViewJavascriptBridge.git", from: "1.2.0")
+        .package(url: "https://github.com/Lision/WKWebViewJavascriptBridge.git", branch: "master")
     ],
     targets: [
         .target(
